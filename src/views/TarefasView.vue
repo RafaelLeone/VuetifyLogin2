a5c02751ace73bab539050f50035f50b7469a2da
RafaelLeone <rinaciodacosta@gmail.com>
2022-09-14 14:38:36 -0300

    #5

--- src/views/TarefasView.vue
+++ src/views/TarefasView.vue
@@ -1,15 +1,24 @@
 <template>
-  <hello-world />
+  <div>
+    <v-list flat subheader three-line>
+      <v-subheader>General</v-subheader>
+
+      <v-list-item-group v-model="settings" multiple active-class="">
+        <TarefaView />
+        <TarefaView />
+      </v-list-item-group>
+    </v-list>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import TarefaView from "../components/tarefas/TarefaView.vue";
 
 export default {
   name: "HomeView",
 
   components: {
-    HelloWorld,
+    TarefaView,
   },
 };
 </script>
